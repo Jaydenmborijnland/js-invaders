ff959ab4c3e4dded878f9c331aba88a60960b7c7
Jaydenmborijnland <6019486@mborijnland.nl>
2022-05-10 09:58:43 +0200

    Update invaders.js

--- invaders.js
+++ invaders.js
@@ -1,11 +1,47 @@
-function setup() {
+let player = {
+    x: 400
+};
+
+
+function draw() {
     let canvas = document.getElementById("invaders-canvas");
     let context = canvas.getContext("2d");
+    
+    context.fillStyle = "black";
+    context.fillRect(0, 0, 800, 600);
     context.font = "48px serif";
-    context.fillText("space invaders", 10, 50);
+    context.fillStyle = "white";
 
-    context.fillRect(390, 580, 20, 20);
+    context.fillText("space invaders", 10, 50);
+    context.fillStyle = "blue";
+    //context.fillRect(390, 580, 20, 20);
+    context.beginPath();
+    context.moveTo(player.x, 580);
+    context.lineTo(player.x - 10, 600);
+    context.lineTo(player.x + 10, 600);
+    context.fill();
 }
 
+function setup() {
+    draw();
+}
+
+function movePlayer(event) {
+    switch(event.key){
+        case "ArrowLeft":
+        player.x -= 10;    
+        break;
+        case "ArrowRight":
+            player.x += 10;
+        break;
+
+    }
+        
+    draw();
+
+
+
+}
 
 window.addEventListener("load", setup);
+window.addEventListener("keydown", movePlayer);
