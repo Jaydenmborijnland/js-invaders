ce4382a7913301c6100bd9b99615bc72fb652ca3
Jaydenmborijnland <6019486@mborijnland.nl>
2022-06-08 10:35:27 +0200

    Update invaders.js

--- invaders.js
+++ invaders.js
@@ -1,47 +1,152 @@
+class Bullet {
+    x;
+    y;
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+
+    update() {
+        this.y -= 10;
+    }
+}
 let player = {
-    x: 400
+    x: 400,
+    y: 580
 };
 
+let bullets = [];
+
+let direction = {
+    left: false,
+    right: false,
+    up: false,
+    down: false,
+};
+
+function update() {
+    if(direction.left == true) {
+        if(player.x > 10) {
+            player.x -= 10;
+        }
+    }
+    if(direction.right == true) {
+        if(player.x < 790) {
+            player.x += 10;
+        }
+    }
+    if(direction.up == true) {
+        if(player.y > 0) {
+            player.y -= 10;
+        }
+    }
+    if(direction.down == true) {
+        if(player.y < 580) {
+            player.y += 10;
+        }
+    }
+
+    // update()
+    for(let index = 0; index < bullets.length; index++) {
+        bullets[index].y -= 10;
+    }
+
+    draw();
+}
 
 function draw() {
-    let canvas = document.getElementById("invaders-canvas");
-    let context = canvas.getContext("2d");
-    
+    let canvas = document.getElementById('invaders-canvas');
+    let context = canvas.getContext('2d');
+
     context.fillStyle = "black";
     context.fillRect(0, 0, 800, 600);
-    context.font = "48px serif";
-    context.fillStyle = "white";
 
-    context.fillText("space invaders", 10, 50);
-    context.fillStyle = "blue";
-    //context.fillRect(390, 580, 20, 20);
+    context.fillStyle = "white";
+    context.font = '48px serif';
+    context.fillText("Space Invaders", 10, 50);
+
+    context.fillStyle = "yellow";
+    // context.fillRect(390, 580, 20, 20);
     context.beginPath();
-    context.moveTo(player.x, 580);
-    context.lineTo(player.x - 10, 600);
-    context.lineTo(player.x + 10, 600);
+    context.moveTo(player.x, player.y);
+    context.lineTo(player.x - 10, player.y + 20);
+    context.lineTo(player.x + 10, player.y + 20);
     context.fill();
+
+    // draw()
+    for(let index = 0; index < bullets.length; index++) {
+        const bullet = bullets[index];
+        context.fillStyle = "red";
+        context.beginPath();
+        context.arc(bullet.x, bullet.y, 5, 0, Math.PI * 2);
+        context.fill();
+    }
+
+    // if(bullet != null) {
+    //     context.fillStyle = "red";
+    //     context.beginPath();
+    //     context.arc(bullet.x, bullet.y, 5, 0, Math.PI * 2);
+    //     context.fill();
+    // }
 }
 
 function setup() {
     draw();
 }
 
-function movePlayer(event) {
-    switch(event.key){
+function keyDown(event) {
+    switch(event.key) {
         case "ArrowLeft":
-        player.x -= 10;    
-        break;
+            direction.left = true;
+            // player.x -= 10;
+            break;
         case "ArrowRight":
-            player.x += 10;
-        break;
+            direction.right = true;
+            // player.x += 10;
+            break;
 
+        case "ArrowUp":
+            direction.up = true;
+            // player.y -= 10;
+            break;
+
+        case "ArrowDown":
+            direction.down = true;
+            // player.y += 10;
+            break;
+
+        case " ":
+            bullets.push({
+                x: player.x,
+                y: player.y
+            });
+            break;
     }
-        
-    draw();
-
-
-
 }
 
-window.addEventListener("load", setup);
-window.addEventListener("keydown", movePlayer);
+function keyUp(event) {
+    switch(event.key) {
+        case "ArrowLeft":
+            direction.left = false;
+            break;
+        case "ArrowRight":
+            direction.right = false;
+            break;
+
+        case "ArrowUp":
+            direction.up = false;
+            break;
+
+        case "ArrowDown":
+            direction.down = false;
+            break;
+    }
+}
+
+window.addEventListener('load', setup);
+window.addEventListener('keydown', keyDown);
+window.addEventListener('keyup', keyUp);
+
+setInterval(update, 50);
